99b30a8b034381ca20cddd045873a72d35634f29
Everton da Rosa <everton3x@gmail.com>
2021-05-10 16:58:59 -0300

    Adicionado origem/lista

---
+++ src/Origem/ListaOrigemCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Kontas\Origem;
+
+/**
+ * Description of OrigemCommand
+ *
+ * @author Everton
+ */
+class ListaOrigemCommand extends \Kontas\Command\CommandAbstract {
+    
+    public function __construct() {
+        parent::__construct('Lista as origens cadastradas.', [
+            'ativos' => [
+                'prefix' => 'a',
+                'longPrefix' => 'ativos',
+                'description' => 'Exibe apenas os ativos.',
+                'noValue' => true
+            ],
+            'inativos' => [
+                'prefix' => 'i',
+                'longPrefix' => 'inativos',
+                'description' => 'Exibe apenas os inativos.',
+                'noValue' => true
+            ]
+        ]);
+    }
+    
+    public function execute(): void {
+        $origem = new Origem();
+        
+        $lista = $origem->lista();
+        
+        if($this->climate->arguments->get('ativos')){
+            $lista = $origem->listaAtivos();
+        }
+        
+        if($this->climate->arguments->get('inativos')){
+            $lista = $origem->listaInativos();
+        }
+        
+        if($this->climate->arguments->get('ativos') && $this->climate->arguments->get('inativos')){
+            $lista = $origem->lista();
+        }
+        
+        $this->climate->flank("Origens cadastradas");
+        
+        $this->climate->table($lista);
+    }
+
+}
