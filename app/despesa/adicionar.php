f5a049964c2ed300acff8569be807f62b2017d7d
Everton da Rosa <everton3x@gmail.com>
2021-05-21 16:20:20 -0300

    despesa/adicionar implementado autopagamento quando mp não é autopagar

--- app/despesa/adicionar.php
+++ app/despesa/adicionar.php
@@ -4,26 +4,25 @@ require_once 'vendor/autoload.php';
 
 $climate = new \League\CLImate\CLImate();
 
-try{
+try {
     $climate->info('Cadastra uma nova despesa...');
 
     $periodo = \kontas\util\periodo::parseInput(\kontas\io\periodo::askPeriodo());
-    
+
     $descricao = \kontas\io\generic::askDescricao();
-    
+
     $aplicacao = \kontas\io\aplicacao::select();
-    
+
     $projeto = \kontas\io\projeto::select();
-    
+
     $agrupador = \kontas\io\generic::askAgrupador();
-    
+
     $valor = \kontas\io\generic::askValor();
-    
+
     $key = \kontas\ds\despesa::addDespesa($periodo, $descricao, $aplicacao, $projeto, '', 1, 1, $valor, date('Y-m-d'), 'Previsão inicial');
-    
-    
+
     $gastar = $climate->confirm('Deseja cadastrar como despesa gasta?');
-    if($gastar->confirmed() === true){
+    if ($gastar->confirmed() === true) {
         $credor = \kontas\io\despesa::askCredor();
         $mp = \kontas\io\mp::select();
         $vencimento = \kontas\io\generic::askVencimento();
@@ -31,18 +30,22 @@ try{
         $valor = \kontas\io\generic::askValor('Valor do gasto [####.##]:');
         $data = \kontas\util\date::parseInput(\kontas\io\generic::askVencimento('Data do gasto [ddmmaaaa]:'));
         $observacao = \kontas\io\generic::askDescricao('Observação do gasto (opcional):');
-        
+
         $gasto = \kontas\ds\despesa::gastar($periodo, $key, $credor, $mp, $vencimento, $cc, $valor, $data, $observacao);
         if (\kontas\ds\mp::isAutopagar($mp) === true) {
             \kontas\ds\despesa::pagar($periodo, $key, $gasto, $valor, $data, 'Autopagamento');
+        } else {
+            $pagar = $climate->confirm('Deseja fazer o pagamento do gasto?');
+            if ($pagar->confirmed() === true) {
+                \kontas\ds\despesa::pagar($periodo, $key, $gasto, $valor, $data, 'Autopagamento');
+            }
         }
     }
-    
-    
-    
+
+
+
     $climate->info("Despesa criada:");
     \kontas\io\despesa::resume($periodo, $key);
-    
 } catch (Exception $ex) {
     $climate->error($ex->getMessage());
     $climate->yellow()->out($ex->getTraceAsString());
