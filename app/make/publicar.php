78664f5223cf052f834721604bc53c7b128e39d9
Everton da Rosa <everton3x@gmail.com>
2021-05-25 15:14:52 -0300

    implementtada geração de aaaa.html

--- app/make/publicar.php
+++ app/make/publicar.php
@@ -14,6 +14,7 @@ try {
 
 
     //index.html
+    $climate->info('Criando index.html');
     $periodos = \kontas\ds\periodo::listAll();
     $data = [];
     foreach ($periodos as $key => $status) {
@@ -46,8 +47,58 @@ try {
     krsort($data['periodos']);
     ksort($data['anos']);
 
-//    print_r($data);exit();
-    kontas\util\template::write('index', $data);
+    kontas\util\template::write('index', 'index', $data);
+    //fim index.html
+    
+    //aaaa.html
+    $climate->info('Criando aaaa.html...');
+    $periodos = \kontas\ds\periodo::listAll();
+    foreach ($periodos as $key => $status) {
+        $dt = DateTime::createFromFormat('Y-m', $key);
+        $anos[$dt->format('Y')][$dt->format('m')] = $key;
+    }
+//    print_r($anos);
+
+    foreach ($anos as $ano => $periodos) {
+        $climate->tab()->out("...$ano.html");
+        $data = [];
+        $data['ano'] = $ano;
+        $data['periodos'] = [];
+        
+        foreach ($periodos as $key => $periodo){
+            $dados = \kontas\ds\periodo::load($periodo);
+            
+            $receitas = 0;
+            foreach ($dados['receitas'] as $item){
+                foreach ($item['previsao'] as $subitem){
+                    $receitas += $subitem['valor'];
+                }
+            }
+            
+            $despesas = 0;
+            foreach ($dados['receitas'] as $item){
+                foreach ($item['previsao'] as $subitem){
+                    $despesas += $subitem['valor'];
+                }
+            }
+            
+            $resultado = $dados['resultados']['periodo'];
+            $acumulado = $dados['resultados']['acumulado'];
+            
+            $dt = DateTime::createFromFormat('Y-m', $periodo);
+            $data['periodos'][$key] = [
+                'nome' => $dt->format('M'),
+                'receitas' => $receitas,
+                'despesas' => $despesas,
+                'resultado' => $resultado,
+                'acumulado' => $acumulado
+            ];
+        }
+//        print_r($data);
+        kontas\util\template::write('aaaa', $ano, $data);
+    }
+
+    //fim aaaa.html
 } catch (Exception $ex) {
     $climate->error($ex->getMessage());
     $climate->yellow()->out($ex->getTraceAsString());
--- src/util/template.php
+++ src/util/template.php
@@ -17,13 +17,13 @@ class template {
         return new \Twig\Environment($loader);
     }
 
-    public static function write(string $tpl, array $data = []): void {
+    public static function write(string $tpl, string $target, array $data = []): void {
         $engine = self::engine(self::KTPL_DIR);
 
         $template = $engine->load("$tpl.twig");
         $html = $template->render($data);
 //        print_r($html);
-        $filename = self::KHTML_DIR.$tpl.".html";
+        $filename = self::KHTML_DIR.$target.".html";
         file_put_contents($filename, $html);
     }
 
