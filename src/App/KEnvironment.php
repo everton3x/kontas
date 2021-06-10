7a883ea5043b90a47db1849f1f09c2a5d96e9ecb
Everton da Rosa <everton3x@gmail.com>
2021-06-10 13:55:40 -0300

    modificado KEnvironment

--- src/App/KEnvironment.php
+++ src/App/KEnvironment.php
@@ -9,7 +9,7 @@ namespace Kontas\App;
  */
 class KEnvironment extends \PTK\Console\Flow\Environment\EnvironmentAbstract {
     
-    protected \PDO $dbh;
+//    protected \PDO $dbh;
     protected string $dbDSN = 'sqlite:data/kontas.db';
     protected null|string $dbUser = null;
     protected null|string $dbPassword = null;
@@ -20,16 +20,18 @@ class KEnvironment extends \PTK\Console\Flow\Environment\EnvironmentAbstract {
     }
     
     protected function prepareDb(): void {
-        $this->dbh = new \PDO($this->dbDSN, $this->dbUser, $this->dbPassword);
-        $this->dbh->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        $this->resources['dbh'] = new \PDO($this->dbDSN, $this->dbUser, $this->dbPassword);
+        $this->resources['dbh']->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+//        $this->dbh = new \PDO($this->dbDSN, $this->dbUser, $this->dbPassword);
+//        $this->dbh->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
     }
     
-    public function __get(string $member): mixed {
-        if(property_exists($this, $member)){
-            return $this->{$member};
-        }
-        
-        throw new ResourceNotFoundException($member);
-    }
+//    public function __get(string $member): mixed {
+//        if(property_exists($this, $member)){
+//            return $this->{$member};
+//        }
+//        
+//        throw new ResourceNotFoundException($member);
+//    }
     
 }
